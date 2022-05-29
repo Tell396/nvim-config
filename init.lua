62988439f18e72b438fb9a579f1c5fa11da6d997
Tell396 <vova21473@gmail.com>
2022-05-29 17:15:03 +1000

    Story #1. Added main files.

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- Packer
+require('plugins')
+require('settings')
+require('settings.color')
+require('settings.font')
+require('settings.completion')
+require('settings.filetype')
+-- LSP Config
+require('lsp')
+-- Keymap
+require('settings.keymap')
---
+++ lua/settings/init.lua
@@ -0,0 +1,47 @@
+-- Global
+vim.opt.fillchars = { vert = ' ' }
+vim.opt.showtabline = 2
+vim.opt.scrolloff = 5
+vim.opt.mouse = 'a'
+vim.opt.backupcopy = 'yes'
+vim.opt.undolevels = 1000
+vim.opt.shortmess:append { c = true, S = true }
+vim.opt.showmode = false
+vim.opt.hidden = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.wrapscan = true
+vim.opt.backup = false
+vim.opt.hlsearch = true
+vim.opt.smartcase = true
+vim.opt.errorbells = false
+vim.opt.joinspaces = false
+vim.opt.title = true
+
+vim.opt.lazyredraw = true
+
+vim.opt.encoding = 'UTF-8'
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.listchars = { tab = ">>>", trail = "·", precedes = "←", extends = "→",eol = "↲", nbsp = "␣" }
+vim.opt.laststatus = 3
+-- Buffer
+vim.opt.fileformat = 'unix'
+vim.opt.tabstop = 4
+vim.opt.spelllang = 'it'
+vim.opt.softtabstop = 4
+vim.opt.swapfile = false
+vim.opt.undofile = false
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+-- Window
+vim.opt.number = true
+vim.opt.colorcolumn = "+1"
+vim.opt.foldmethod = 'indent'
+vim.opt.foldlevel = 1
+vim.opt.list = false
+vim.opt.foldnestmax = 10
+vim.opt.signcolumn = 'yes'
+vim.opt.relativenumber = false
+vim.opt.foldenable = false
+vim.opt.cursorline = true
